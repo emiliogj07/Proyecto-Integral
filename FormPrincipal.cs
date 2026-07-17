01bf20a9bd2fc0717893c83cdc870f4ca3fa528b
miguelon2602 <miguelangelabreu26@gmail.com>
2026-07-16 21:02:32 -0400

    Update FormPrincipal.cs

--- FormPrincipal.cs
+++ FormPrincipal.cs
@@ -28,6 +28,13 @@ public void Iniciar()
         Console.WriteLine("Error: Los límites deben ser números.");
         return;
     }
+         EvaluadorFunciones evaluador = new EvaluadorFunciones();   //Crea un objeto 'EvaluadorFunciones'
+        var funcionEvaluada = evaluador.ObtenerFuncion(funcion);   //Traduce el texto que escribio el usuario ("x^2", "sin(x)", "cos(x)") en una funcion matematica real que se puede evaluar
+
+        CalculadoraIntegral calc = new CalculadoraIntegral(); // Crea un objeto 'CalculadoraIntegral'
+        double resultado = calc.CalcularTrapecio(funcionEvaluada, a, b, 100);  // Llama al metodo CalcularTrapecio para aproximar la integral de la funcion entre los limites a y b
+                                                                                     //Usa 100 subdivisiones para la aproximación.
+        Console.WriteLine($"Resultado aproximado: {resultado}");
 
   }
 }
