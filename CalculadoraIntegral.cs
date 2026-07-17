b75bb75c3a52739e36247f3b7f55f7a292ea6cfb
miguelon2602 <miguelangelabreu26@gmail.com>
2026-07-16 20:56:13 -0400

    Update CalculadoraIntegral.cs

--- CalculadoraIntegral.cs
+++ CalculadoraIntegral.cs
@@ -1,10 +1,21 @@
-namespace Proyecto_Integral
+using System;
+
+namespace Proyecto_Integral 
 {
     public class CalculadoraIntegral
     {
-        public double Calcular(string funcion, double a, double b)
+        public double CalcularTrapecio(Func<double, double> funcion, double a, double b, int n)
         {
-            return 0;
+            double h = (b - a) / n;     // Calculamos el ancho de cada intervalo
+            double suma = (funcion(a) + funcion(b)) / 2.0;  //aqui se inicializa la suma con los extremos de la función
+
+            for (int i = 1; i < n; i++)  //Esto es parte de la fórmula del método del trapecio.
+            {
+                double xi = a + i * h;
+                suma += funcion(xi);
+            }
+
+            return h * suma;    //Multiplica la suma por el ancho h
         }
     }
 }
